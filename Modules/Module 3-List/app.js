f911a4e1144e3607080f932bd5777611260742a5
Monte McNeil <monte.mcneil27@gmail.com>
2021-03-04 12:19:07 -0600

    Complete

--- Modules/Module 3-List/app.js
+++ Modules/Module 3-List/app.js
@@ -1,9 +1,8 @@
-
 function validateThenAdd() {
     var newWord = document.forms["theForm"]["word"].value
     var newNumber = document.forms["theForm"]["number"].value
 
-    if (newWord == " ") {
+    if (newWord == "") {
         alert("Enter a word to check")
         return false
     }
@@ -15,26 +14,31 @@ function validateThenAdd() {
         return false
     }
     else {
+
         if(newNumber == 1) {
-            var table = document.getElementById("list1")
-            (table.insertRow(table.rows.length)).innerHTML = newWord
+            var tableRef = document.getElementById("list1");
+            (tableRef.insertRow(tableRef.rows.length)).innerHTML = newWord;
+
         }
+
             else {
-            var table = document.getElementById("list2")
-            (table.insertRow(table.rows.length)).innerHTML = newWord
+                var tableRef = document.getElementById("list2");
+                (tableRef.insertRow(tableRef.rows.length)).innerHTML = newWord;
+
+                document.forms["theForm"]["newWord"].value = "";
+                document.forms["theForm"]["newNumber"].value = "";
+                return true;
             }
 
-            document.forms["theForm"]["newWord"].value = ""
-            document.forms["theForm"]["newNumber"].value = ""
         }
 }
 
 function clearList1() {
-    var table1 = document.getElementById("list1")
-    table1.innerHTML = " ";
+    var tableRef = document.getElementById("list1")
+    tableRef.innerHTML = " ";
 }
 
 function clearList2() {
-    var table2 = document.getElementById("list2")
-    table2.innerHTML = " ";
+    var tableRef = document.getElementById("list2")
+    tableRef.innerHTML = " ";
 }
