921bb899e02f4397d79d87624fe492f6466ab4b1
Monte McNeil <monte.mcneil27@gmail.com>
2021-05-06 15:35:38 -0500

    done

---
+++ Modules/Module 7-Project/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <>
+          <div className="font-mono flex bg-blue-500 text-4xl py-10 ">
+              <header>Welcome to Launch Library 2!</header>
+          </div>
+        </>
+      );
+    };
