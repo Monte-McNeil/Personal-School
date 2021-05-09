6f7bdd2c75ae7380945cae7e2453c4f844e26d75
Monte McNeil <monte.mcneil27@gmail.com>
2021-05-09 14:48:32 -0500

    done

--- Modules/Module 7-Project/pages/index.js
+++ Modules/Module 7-Project/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Launch Library 2</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Navbar />
---
+++ Modules/react-survey/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+   
+      </div>
+    );
+  }
+}
+
+export default App;
