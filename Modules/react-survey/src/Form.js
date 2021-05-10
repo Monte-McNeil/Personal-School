5813f37bd7dbc4d36e3294937e0c38edd926b387
Monte McNeil <monte.mcneil27@gmail.com>
2021-05-10 16:44:48 -0500

    Done

--- Modules/react-survey/src/Form.js
+++ Modules/react-survey/src/Form.js
@@ -5,9 +5,11 @@ import React from 'react'
     super(props);
 
     this.state = {
-      fullName: '',
-      sport: '',
-      feedback: '',
+      fullName: ' ',
+      gender: ' ',
+      sport: ' ',
+      enjoy: 'yes',
+      feedback: ' '
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,12 +23,15 @@ import React from 'react'
     this.setState({
       [name]: value,
   });
-    console.log(this.state)
 }
 
   handleSubmit(e) {
     e.preventDefault();
-    alert('Name is' + this.state.fullName +  ' Sport is: ' + this.state.sport +  ' Feedback is: ' + this.state.feedback)
+    alert('Name is' + this.state.fullName +
+    ' | Gender is: ' + this.state.gender +
+    ' | Sport is: ' + this.state.sport +
+    ' | Enjoy is' + this.state.enjoy +
+    ' | Feedback is: ' + this.state.feedback)
   }
 
   render() {
@@ -40,11 +45,41 @@ import React from 'react'
         </label> <br></br>
 
         <label>
+          What is your gender?
+          <select required name="gender"
+          value={this.state.gender}
+          onChange={this.handleChange}>
+             <option value="man">Man</option>
+             <option value="woman">Women</option>
+             <option value="differ">Prefer not to say</option>
+          </select>
+        </label>
+
+        <label>
           What sport do you play?
           <input type="text" required name="sport" 
           value={this.state.sport} 
           onChange={this.handleChange} />
         </label> <br></br>
+
+        Did you enjoy your sport?
+        Yes
+        <input
+           type="radio"
+           name="enjoy"
+           value="yes"
+           checked={this.state.enjoy === "yes"}
+           onChange={this.handleChange}
+        />
+
+        No
+        <input
+           type="radio"
+           name="enjoy"
+           value="no"
+           checked={this.state.enjoy === "no"}
+           onChange={this.handleChange}
+        /> <br></br>
 
         <label>
           Please provide feedback about your Doane athletic experience <br></br>
